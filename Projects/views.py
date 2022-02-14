ce77b86df1a77b6798fe11a4bd7f7c8fc1b02b74
lmkr1409 <manojkumar.lakkireddy@gmail.com>
2022-02-14 22:23:51 +0530

    optimized imports

--- Projects/views.py
+++ Projects/views.py
@@ -1,17 +1,13 @@
 from django.shortcuts import render
 from django.views import View
 from django.shortcuts import render
-import markdown
 
-import os
-from resume.settings import BASE_DIR
 # Create your views here.
 
 
 class BaseDoc(View):
     
     def get(self, request):
-        # base = markdown.markdown(open(os.path.join(BASE_DIR, "Projects", "Static", "PythonDoc", "base.md")).read())
         return render(request, 'PythonNotes/base.html')
 
 
